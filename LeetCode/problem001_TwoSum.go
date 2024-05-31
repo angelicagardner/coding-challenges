f7e23e90fe7039d883be10d64dc585be4f4f30b7
Angelica Gardner <23157778+angelicagardner@users.noreply.github.com>
2024-05-31 21:31:05 +0200

    feat: add LeetCode TwoSum problem solutions and test cases

---
+++ LeetCode/problem001_TwoSum.go
@@ -0,0 +1,33 @@
+/*
+Problem 1. Two Sum
+*/
+
+package leetcode
+
+// Time complexity:		O(n^2)
+// Space complexity: 	O(1)
+func TwoSumBruteForce(nums []int, target int) []int {
+	for i := 0; i < len(nums); i++ {
+		for j := i + 1; j < len(nums); j++ {
+			if nums[i]+nums[j] == target {
+				return []int{i, j}
+			}
+		}
+	}
+	return nil
+}
+
+// Time complexity:		O(n)
+// Space complexity: 	O(n)
+func TwoSumHashMap(nums []int, target int) []int {
+	hashMap := make(map[int]int)
+	for i, num := range nums {
+		complement := target - num
+		complementIndex, found := hashMap[complement]
+		if found {
+			return []int{complementIndex, i}
+		}
+		hashMap[num] = i
+	}
+	return nil
+}
