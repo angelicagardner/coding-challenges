792c7e7e88be7226c5d12086b8e2027a31558471
Angelica Gardner <23157778+angelicagardner@users.noreply.github.com>
2024-06-12 21:23:31 +0200

    style: fix linting errors in problem009_PalindromeNumber.go

---
+++ LeetCode/problem009_PalindromeNumber.go
@@ -0,0 +1,33 @@
+/* Problem 9. Palindrome Number */
+package leetcode
+
+import (
+	"math"
+)
+
+func isPalindrome(x int) bool {
+	// Negative numbers are not palindromes
+	if x < 0 {
+		return false
+	}
+
+	original := x
+	reversed := 0
+
+	// Reversing the number
+	for x != 0 {
+		// Pop the last digit from x
+		pop := x % 10
+		x /= 10
+
+		// Check for overflow (as reversing might cause overflow)
+		if reversed > (math.MaxInt32-pop)/10 {
+			return false
+		}
+		// Push the digit to the reversed number
+		reversed = reversed*10 + pop
+	}
+
+	// Check if the original number is the same as the reversed number
+	return original == reversed
+}
