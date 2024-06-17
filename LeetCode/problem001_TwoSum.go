ce99490294e459379d5c4281b542da51dbe953cc
Angelica Gardner <23157778+angelicagardner@users.noreply.github.com>
2024-06-17 23:11:56 +0200

    feat: add romanToInteger solution

--- LeetCode/problem001_TwoSum.go
+++ LeetCode/problem001_TwoSum.go
@@ -6,7 +6,7 @@ package leetcode
 
 // Time complexity:  O(n^2)
 // Space complexity: O(1)
-func TwoSumBruteForce(nums []int, target int) []int {
+func twoSumBruteForce(nums []int, target int) []int {
 	for i := 0; i < len(nums); i++ {
 		for j := i + 1; j < len(nums); j++ {
 			if nums[i]+nums[j] == target {
@@ -19,7 +19,7 @@ func TwoSumBruteForce(nums []int, target int) []int {
 
 // Time complexity:  O(n)
 // Space complexity: O(n)
-func TwoSumHashMap(nums []int, target int) []int {
+func twoSumHashMap(nums []int, target int) []int {
 	hashMap := make(map[int]int)
 	for i, num := range nums {
 		complement := target - num
