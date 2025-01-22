9316644b103dcb11a2325377d58828582df5cb0b
Angelica Hjelm Gardner <angelica.hjelm.gardner@ericsson.com>
2025-01-22 16:35:44 +0100

    feat(Leetcode #27): remove specified element in-place from int array

---
+++ LeetCode/problem027_RemoveElement.go
@@ -0,0 +1,15 @@
+package leetcode
+
+// Time complexity:  O(n)
+// Space complexity: O(1)
+func RemoveElement(nums []int, val int) int {
+	k := 0
+
+	for i := 0; i < len(nums); i++ {
+		if nums[i] != val {
+			nums[k] = nums[i]
+			k++
+		}
+	}
+	return k
+}
