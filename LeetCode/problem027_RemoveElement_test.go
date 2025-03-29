fcbe4388e4e335ccfcd2b86b43b0f128706ff1e1
Angelica Hjelm Gardner <angelica.hjelm.gardner@ericsson.com>
2025-03-29 16:14:53 +0100

    feat(LeetCode #28): find the index of the first occurrence in a string

--- LeetCode/problem027_RemoveElement_test.go
+++ LeetCode/problem027_RemoveElement_test.go
@@ -12,62 +12,14 @@ func TestRemoveElement(t *testing.T) {
 		expectedLength int
 		expectedNums   []int
 	}{
-		{
-			name:           "Example 1",
-			nums:           []int{3, 2, 2, 3},
-			val:            3,
-			expectedLength: 2,
-			expectedNums:   []int{2, 2},
-		},
-		{
-			name:           "Example 2",
-			nums:           []int{0, 1, 2, 2, 3, 0, 4, 2},
-			val:            2,
-			expectedLength: 5,
-			expectedNums:   []int{0, 1, 4, 0, 3},
-		},
-		{
-			name:           "All elements to remove",
-			nums:           []int{7, 7, 7, 7},
-			val:            7,
-			expectedLength: 0,
-			expectedNums:   []int{},
-		},
-		{
-			name:           "No elements to remove",
-			nums:           []int{1, 2, 3, 4},
-			val:            5,
-			expectedLength: 4,
-			expectedNums:   []int{1, 2, 3, 4},
-		},
-		{
-			name:           "Empty array",
-			nums:           []int{},
-			val:            1,
-			expectedLength: 0,
-			expectedNums:   []int{},
-		},
-		{
-			name:           "Single element not to remove",
-			nums:           []int{1},
-			val:            2,
-			expectedLength: 1,
-			expectedNums:   []int{1},
-		},
-		{
-			name:           "Single element to remove",
-			nums:           []int{1},
-			val:            1,
-			expectedLength: 0,
-			expectedNums:   []int{},
-		},
-		{
-			name:           "Multiple occurrences scattered",
-			nums:           []int{4, 5, 6, 4, 7, 4, 8},
-			val:            4,
-			expectedLength: 4,
-			expectedNums:   []int{5, 6, 7, 8},
-		},
+		{"Example 1", []int{3, 2, 2, 3}, 3, 2, []int{2, 2}},
+		{"Example 2", []int{0, 1, 2, 2, 3, 0, 4, 2}, 2, 5, []int{0, 1, 4, 0, 3}},
+		{"All elements to remove", []int{7, 7, 7, 7}, 7, 0, []int{}},
+		{"No elements to remove", []int{1, 2, 3, 4}, 5, 4, []int{1, 2, 3, 4}},
+		{"Empty array", []int{}, 1, 0, []int{}},
+		{"Single element not to remove", []int{1}, 2, 1, []int{1}},
+		{"Single element to remove", []int{1}, 1, 0, []int{}},
+		{"Multiple occurrences scattered", []int{4, 5, 6, 4, 7, 4, 8}, 4, 4, []int{5, 6, 7, 8}},
 	}
 
 	for _, tt := range tests {
